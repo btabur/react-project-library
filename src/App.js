91b7fc19bca980ad68ec8169b1c1fc2ba4db441f
Balı Tabur <webtasarim.0323@gmail.com>
2023-09-19 14:33:18 +0300

    change readed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import BookCard from "./components/bookCard"
+import {toast} from "react-toastify";
 
 function App() {
   const [bookName, setBookName] = useState("");
 
   const [bookList,setBookList]=useState([]);
+  const [showConfirm,setShowConfirm]=useState(false);
+  const [deletingId,setDeleteId]=useState(null);
 
   const addBook = (e) => {
     e.preventDefault();
@@ -18,15 +21,38 @@ function App() {
 
     setBookList([newBook,...bookList]) //önceki kitapların başına yeni bir kitap eklemek için
     setBookName('')
+
+    toast.success("Kitap Eklendi",{autoClose:2000})
   };
  
+  const handleModal =(id)=> {
+      setShowConfirm(true);
+      setDeleteId(id)
+  }
+
   const handleDelete = (deleteId)=> {
 
     const filteredList= bookList.filter(book => book.id !==deleteId)
     setBookList(filteredList)
 
+    toast.error("Kitap Silindi", {autoClose:2000})
   }
+  const handleRead =(book)=> {
 
+    const updatedBook = {...book,isRead:!book.isRead} //okundu değerini tersine çevirir
+
+    const cloneBooks = [...bookList];
+   
+    //findIndex Metodu
+    //kitabın index değerini bulur.
+   const index= cloneBooks.findIndex((item)=>item.id ===book.id); 
+  
+
+   cloneBooks.splice(index,1,updatedBook); //booklist dizisinde index nolu elemanı siler yerine yeni kitabı koyar
+   setBookList(cloneBooks)
+
+  
+  }
   return (
     <div className="App">
       <header className="bg-dark p-2 text-white text-center fs-5">
@@ -50,14 +76,26 @@ function App() {
             bookList.map(book=>{
               return(
                 <BookCard
-                handleDelete={handleDelete}
-                bookInfo={book}/>
+                key={book.id}
+                handleModal={handleModal}
+                handleRead = {handleRead}
+                book={book}/>
               );
              
             })
           }
         </div>
       </div>
+      {/* Modal tanımlama */}
+      {showConfirm &&
+        <div >
+          <h5>Silmek istiyormusunuz ?</h5>
+          <button onClick={()=>setShowConfirm(false)}>Vazgeç</button>
+          <button onClick={()=>{ 
+            handleDelete(deletingId); setShowConfirm(false);
+            }}>Onayla</button>
+        </div>
+      }
     </div>
   );
 }
--- src/components/bookCard.js
+++ src/components/bookCard.js
@@ -1,19 +1,23 @@
-const bookCard = ({bookInfo, handleDelete}) => {
-    console.log('book card', bookInfo)
+const bookCard = ({book, handleDelete,handleRead,handleModal}) => {
+  
 
     return(
         <div className="d-flex align-items-center justify-content-between border rounded shadow p-2">
             {/* Sol taraf */}
             <div>
-                <h5>{bookInfo.bookTitle}</h5>
-                <p>{bookInfo.date
+                <h5 style={{
+                    textDecoration: book.isRead ? 'line-through':'none'
+                }}>{book.bookTitle}</h5>
+                <p>{book.date
                }</p>
             </div>
             {/* sağ taraf */}
             <div className="btn-group">
-                <button className="btn btn-danger" onClick={()=>handleDelete(bookInfo.id)}>Sil</button>
+                <button className="btn btn-danger" onClick={()=>handleModal(book.id)}>Sil</button>
                 <button className="btn btn-primary">Düzenle</button>
-                <button className="btn btn-success">Okunmadı</button>
+                <button className="btn btn-success" onClick={()=> handleRead(book)}>
+                    {book.isRead ? "Okundu":"Okunmadı"}
+                </button>
             </div>
 
 
